a365ef7db38b57d9a290ae10c3721c188f4ff872
Oscar L <oscar.lucena@wheelhub.es>
2021-04-22 10:08:29 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>srcwdq/App.js</code> and save to reload. SISOMA IL MUSTAFA
         </p>
         <a
           className="App-link"
